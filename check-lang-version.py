73401170dbf890de9dc7e20fafb4b5ec990d265c
ny1030 <ny1030biz@gmail.com>
2023-07-25 14:11:32 +0900

    add csv output and ignore file

--- check-lang-version.py
+++ check-lang-version.py
@@ -2,6 +2,8 @@ import boto3
 import re
 import subprocess
 import json
+import csv
+from datetime import datetime
 
 def list_matching_ecr_images(account_id, repository_pattern, tag_pattern, profile_name):
     # Create an ECR client by specifying a profile
@@ -39,33 +41,41 @@ def pull_docker_image(image_uri):
     cmd = f"docker pull {image_uri}"
     subprocess.run(cmd, shell=True)
 
-def syft_analyze(image_uri):
+def syft_analyze(image_uri, csv_writer):
     # Execute syft command
     cmd = f"syft packages {image_uri} -o json"
     result = subprocess.run(cmd, capture_output=True, text=True, shell=True)
 
-    # Parsing syft results into JSON format
     output_json = result.stdout.strip()
     if output_json:
         try:
             packages_data = json.loads(output_json)
-            print("Syft analysis result:")
             # Narrow down to matches where artifacts[].name is node or go or java
-            filtered_packages = [{'name': pkg['name'], 'version': pkg['version']} for pkg in packages_data.get('artifacts', []) if 'name' in pkg and pkg['name'] in ['java', 'go', 'node'] and 'version' in pkg]
-            print(json.dumps({'artifacts': filtered_packages}, indent=2))
+            filtered_packages = [{'name': pkg['name'], 'version': pkg['version']} for pkg in packages_data.get('artifacts', []) if 'name' in pkg and pkg['name'] in ['node', 'go', 'java'] and 'version' in pkg]
+            for pkg in filtered_packages:
+                csv_writer.writerow({'image_uri': image_uri.replace(f"{account_id}.dkr.ecr.ap-northeast-1.amazonaws.com/", ""), 'name': pkg['name'], 'version': pkg['version']})
         except json.JSONDecodeError as e:
             print("JSONデータのパースに失敗しました。")
             print(e)
 
 # Get ECR image by specifying pattern
-account_id = '12345678' #FIXME
-repository_pattern = r'test'  # Specify a partial match pattern
+account_id = '599453524280'
+repository_pattern = r'core-eva'  # Specify a partial match pattern
 tag_pattern = r'^latest$'
 profile_name = 'frit'  # Specify the profile name configured in AWS CLI
 matching_images = list_matching_ecr_images(account_id, repository_pattern, tag_pattern, profile_name)
 
-# Pull Docker image and run syft command
-for image_uri in matching_images:
-    print(f"Processing image: {image_uri}")
-    pull_docker_image(image_uri)
-    syft_analyze(image_uri)
+# Output results to CSV file
+today = datetime.today().strftime('%Y%m%d')
+csv_file_name = f"{account_id}_{today}.csv"
+
+with open(csv_file_name, 'w', newline='') as csvfile:
+    fieldnames = ['image_uri', 'name', 'version']
+    csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+    # Pull Docker image and run syft command
+    for image_uri in matching_images:
+        print(f"Processing image: {image_uri}")
+        pull_docker_image(image_uri)
+        syft_analyze(image_uri, csv_writer)
